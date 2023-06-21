d1ff61b668b34d96c045cb2ad662581d00c06285
Saúl <saulespanaara3@gmail.com>
2023-06-21 13:37:24 +0200

    Styles, favicon, and more

---
+++ javascript.js
@@ -0,0 +1,6 @@
+function scrollToSection(event,sectionId) {   
+    event.preventDefault();
+
+    const section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior:'smooth' });
+}
